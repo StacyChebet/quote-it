0c6e6ea93de787caf296ab964d45da1e352fe48a
StacyChebet <staceychebet23@gmail.com>
2018-10-25 09:28:48 +0300

    Add quotes array to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import {Quote} from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  goals = [
+    new Quote ('Stacy Chebet', 'Without the night, we can\'t see the stars', \'Bella, Twilight Saga'),
+    new Quote ('Tracy Kimathi', 'There is no end to the pain, you must be numb', 'XXXTentacion'),
+    new Quote ('Antonia Waweru', 'The future belongs to those who prepare for it today', 'Malcolm X'),
+    new Quote ('Angela Nzioki', 'Our lives begin to end the day we become silent about things that matter', 'Martin Luther King Jr'),
+
+];
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from './quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
+
+  ]
 
   constructor() { }
 
