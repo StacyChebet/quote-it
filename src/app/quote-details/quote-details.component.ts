1a2fa227beb6a516bccdf8c99f2c59b8f0a466e7
StacyChebet <staceychebet23@gmail.com>
2018-10-25 10:35:44 +0300

    add goal delete function

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Quote} from '../quote';
 
 @Component({
@@ -9,6 +9,12 @@ import {Quote} from '../quote';
 export class QuoteDetailsComponent implements OnInit {
 
   @Input () quote: Quote;
+  @Output () isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete: boolean ) {
+    this.isComplete.emit(complete);
+  }
+
 
   constructor() { }
 
