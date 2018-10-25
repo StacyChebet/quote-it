957af22ce7e4f99360fa243a1e20e0d1189ce90c
StacyChebet <staceychebet23@gmail.com>
2018-10-25 09:47:59 +0300

    add name and author of quote in quote-details html file

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input () quote: Quote;
 
   constructor() { }
 
