329e0a418969458cefa03a8fadac19ecb84f043f
StacyChebet <staceychebet23@gmail.com>
2018-10-25 09:38:26 +0300

    Move quote template to quote.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from './quote';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +7,14 @@ import {Quote} from './quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes = [
 
-  ]
+  quotes = [
+    new Quote ('Stacy Chebet', 'Without the night, we can\'t see the stars', 'Bella, Twilight Saga'),
+    new Quote ('Tracy Kimathi', 'There is no end to the pain, you must be numb', 'XXXTentacion'),
+    new Quote ('Antonia Waweru', 'The future belongs to those who prepare for it today', 'Malcolm X'),
+    new Quote ('Angela Nzioki', 'Our lives begin to end the day we become silent about things that matter', 'Martin Luther King Jr'),
+
+];
 
   constructor() { }
 
